21a962be3b1096009899bb341be77c3d031d224c
Caio Balduino <caiobalduinodev@hotmail.com>
2024-10-10 15:45:25 -0300

    🔧 chore: update mobile menu conditions scroll

--- script/menu-mobile.js
+++ script/menu-mobile.js
@@ -3,14 +3,17 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
+  const body = document.body; 
 
   if (menuButton) {
     function openMenu(event) {
       if (menuList.classList.contains("active")) {
         menuButton.classList.remove("animate");
         menuButton.classList.remove("active");
-
         menuList.classList.remove("active");
+
+        body.classList.remove("no-scroll"); // Permite scroll
+
         setTimeout(() => {
           menuList.style.visibility = "hidden";
         }, 400); 
@@ -20,18 +23,22 @@ export default function initMenuMobile() {
         menuList.style.visibility = "visible";
         menuList.classList.add("active");
 
+        body.classList.add("no-scroll"); // Impede o scroll
+
         outsideClick(menuList, ["click", "touchstart"], () => {
           menuButton.classList.remove("animate");
           menuButton.classList.remove("active");
-
           menuList.classList.remove("active");
+
+          body.classList.remove("no-scroll"); // Permite scroll
+
           setTimeout(() => {
             menuList.style.visibility = "hidden";
           }, 400);
         });
       }
     }
-    
+
     menuButton.addEventListener("click", openMenu);
   }
 }
